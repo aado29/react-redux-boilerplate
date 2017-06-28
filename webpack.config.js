361b3cdd06fd03a0f24759c8e3dfa64b03e23f35
Alberto Diaz <aado29@gmail.com>
2017-06-27 21:13:44 -0400

    added source map

--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,8 @@ module.exports = {
 		path: path.resolve(__dirname, "dist"),
 		filename: "bundle.js",
 		publicPath: "public",
-		sourceMapFilename: "bundle.map"
 	},
+	devtool: 'source-map',
 	resolve: {
 		extensions: [".js", ".jsx"]
 	},
